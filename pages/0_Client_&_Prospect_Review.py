c44f9bdb56dc1f4f0d84ad35cab1868d5a8352f3
adifamiology <161519514+adifamiology@users.noreply.github.com>
2024-03-25 20:05:17 +0000

    added state column in excel and in Bar graphs

---
+++ pages/0_Client_&_Prospect_Review.py
@@ -0,0 +1,101 @@
+# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import streamlit as st
+import plotly.express as px #pip install plotly_express 
+import pandas as pd  #pip install pandas
+import os
+import warnings
+warnings.filterwarnings('ignore')
+
+st.set_page_config(page_title="Client Review Dashboards", page_icon=":bar_chart:", layout="wide")
+st.title("Client & Prospect Review")
+st.subheader("Are we targeting right professional communities?")
+st.markdown('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html=True)
+
+#fl = st.file_uploader(":file_folder: Upload a data file", type=(["csv", "xlsx", "xls"]))
+
+#if fl is not None:
+#    filename = fl.name
+#    st.write(filename)
+    #df = pd.read_excel(filename)
+    
+df = pd.read_excel(io='FamilyOfficeEntityDataSampleV1.1.xlsx',
+                engine='openpyxl',
+                       sheet_name='Client Profile' )
+    
+    #print(df)
+
+col1, col2 = st.columns((2))
+df["Date of Birth"] = pd.to_datetime(df["Date of Birth"])
+startDate = pd.to_datetime(df["Date of Birth"]).min()
+endDate = pd.to_datetime(df["Date of Birth"]).max()
+
+with col1:
+        date1 = pd.to_datetime(st.date_input("Start Date", startDate))
+      
+with col2:
+        date2 = pd.to_datetime(st.date_input("End Date", endDate))
+
+df = df[(df["Date of Birth"] >= date1) & (df["Date of Birth"] <= date2)]
+
+st.sidebar.header("Choose your filter: ")
+
+#create for Status
+status = st.sidebar.multiselect("Pick the Status", df["Status"].unique())
+if not status:
+       df2 = df.copy()
+else:
+      df2 = df[df["Status"].isin(status)]
+
+state = st.sidebar.multiselect("Pick the State", df2["State"].unique())
+if not status:
+       df3 = df2.copy()
+else:
+      df3 = df2[df2["State"].isin(state)]
+
+profession = st.sidebar.multiselect("Pick the Profession", df3["Profession"].unique())
+if not profession:
+       df4 = df3.copy()
+else:
+      df4 = df3[df3["Profession"].isin(profession)]
+
+if not status and not state and not profession:
+       filtered_df = df
+elif  not state and not profession:
+       filtered_df = df[df["Status"].isin(status)]
+elif not status and not profession:
+       filtered_df = df[df["State"].isin(state)]
+elif state and profession:
+       filtered_df = df4[df4["State"].isin(state) & df4["Profession"].isin(profession)]
+elif status and profession:
+       filtered_df = df4[df4["Status"].isin(status) & df4["Profession"].isin(profession)]
+elif status and state:
+       filtered_df = df3[df3["Status"].isin(status) & df3["State"].isin(state)]
+
+status_df =  filtered_df.groupby(by = ["Status","State"], as_index = False)["Net Worth"].sum()
+
+with col1:
+       st.subheader("Status wise Net Worth review")
+       fig = px.bar(status_df, x = "Status", y = 'Net Worth', text='State',
+                    template="seaborn")
+       
+       st.plotly_chart(fig, use_container_width=True, height = 200)
+
+profession_df =  filtered_df.groupby(by = ["Profession","State"], as_index = False)["Net Worth"].sum()
+with col2:
+       st.subheader("Profession wise Net Worth review")
+       fig = px.pie(profession_df, values = "Net Worth", names = "Profession", hole=0.5) 
+       fig.update_traces(text = filtered_df["Profession"], textposition = "outside")  
+       st.plotly_chart(fig, use_container_width=True) 
